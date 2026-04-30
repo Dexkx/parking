31ee4958447b66f00d7c9ca0c87010d48c317b47
Dexkx <dexkardhd@gmail.com>
2026-04-29 22:28:44 -0500

    adecuar pk compuestas

--- clientes/models/clientes.py
+++ clientes/models/clientes.py
@@ -2,7 +2,7 @@ from core.models import ModelCore, Usuarios
 from django.db import models
 
 class ClienteNegocio(ModelCore):
-    pk = models.CompositePrimaryKey('negocio', 'usuario')
+    pk = models.CompositePrimaryKey('negocio_id', 'usuario_id')
     negocio = models.ForeignKey('negocios.Negocio', related_name='clientes', on_delete=models.DO_NOTHING)
     usuario = models.ForeignKey(Usuarios, related_name='cliente_negocio', on_delete=models.PROTECT)
 
