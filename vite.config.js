ce6e26cbafb7dafaa8a50b21c16adef8bf4ee54c
Dexkx <dexkardhd@gmail.com>
2026-04-29 22:38:11 -0500

    adecuar .env

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
-  },
-  server: {
-    proxy: {
-      '/api': { target: 'http://localhost:8000', changeOrigin: true }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+    },
+    server: {
+      proxy: {
+        '/api': { target: env.VITE_BACKEND_URL, changeOrigin: true }
+      }
     }
   }
 })
